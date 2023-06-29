68162c27fb9723f1809126eb6f5cf460cd7aa8d3
Jeremiah Griffin <jeremiahgriffin@protonmail.com>
2023-06-29 13:34:53 -0400

    added error field to placenameid

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
-///A tuple containing Id 0 the natural language name of the place
-///id 1 the id associated with that name.
-#[repr(C)]
 pub struct PlaceNameId{
+    pub ie_err: bool,
     pub name: String,
     pub id: String,
 }
+
