f5a7d0c0e48620c8461198194f0b26100d0fe07d
Jeremiah Griffin <jeremiahgriffin@protonmail.com>
2023-06-29 15:03:02 -0400

    switched to using LinkedHashMap

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
+use linked_hash_map::LinkedHashMap;
+
+///if is_err map value must be empty.
+/// if not, the map is keyed by place names, and the values are that place's ID.
 pub struct PlaceNameId{
-    pub ie_err: bool,
-    pub name: String,
-    pub id: String,
+    pub is_err: bool,
+    pub names_ids: LinkedHashMap<String, String>,
 }
 
