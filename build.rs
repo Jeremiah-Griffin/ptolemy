135f56e3879303a35888844eccf29eb017d0eafd
Jeremiah Griffin <jeremiahgriffin@protonmail.com>
2023-06-29 10:49:07 -0400

    removed cbindgen as dependency

--- build.rs
+++ build.rs
@@ -1,12 +1,7 @@
 use std::env;
-use cbindgen;
 
 fn main(){
 
 
-
-    cbindgen::generate("../ptolemy")
-        .expect("!!!New bindings were NOT generated!!!")
-        .write_to_file("headers/ptolemy.h");
 
 }
