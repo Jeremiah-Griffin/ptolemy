a3c37908c7d1a70ab4fccf00a414ad2edf997a6c
Jeremiah Griffin <jeremiahgriffin@protonmail.com>
2023-06-29 15:25:40 -0400

    removed linked has map dependency, switched to vec

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 
-use linked_hash_map::LinkedHashMap;
 
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
 ///if is_err map value must be empty.
 /// if not, the map is keyed by place names, and the values are that place's ID.
 ///
@@ -9,15 +11,15 @@ use linked_hash_map::LinkedHashMap;
 /// I'd rather wasted bandwidth than a failed assertion.
 pub struct PlaceNameId{
     pub is_err: bool,
-    pub names_ids: LinkedHashMap<String, String>,
+    pub names_ids: Vec<(String, String)>,
 }
 
 impl PlaceNameId{
     ///Check struct documentation explaining why buf isn't an Option<T>
-    pub fn new(is_err: bool, buf: &[(String, String)]) -> PlaceNameId {
+    pub fn new(is_err: bool, buf: impl Into<Vec<(String, String)>>) -> PlaceNameId {
         PlaceNameId{
             is_err,
-            names_ids: LinkedHashMap::from_iter(buf.iter().map(|(n, i)| (n.to_string(), i.to_string()))),
+            names_ids: buf.into(),
         }
     }
 }
