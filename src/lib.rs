ed925daafaa45ba2fb8b09a041bef3d63312e8d3
Jeremiah Griffin <jeremiahgriffin@protonmail.com>
2023-07-01 13:11:18 -0400

    added ThothAck type

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,16 @@ pub trait PtolemyFallible {
     fn error() -> Self;
 }
 
+
+#[derive(Serialize, Deserialize)]
+///Actix requires a response no matter what. The value
+/// of "val" is somewhat up to the caller of this function. Usually,
+/// it should be that true: is a successful acceptance of the previous request, and false
+/// is some sort of "soft" failure.
+pub struct ThothAck{
+    val: bool,
+}
+
 #[derive(Serialize, Deserialize)]
 ///if is_err map value must be empty.
 /// if not, the map is keyed by place names, and the values are that place's ID.
