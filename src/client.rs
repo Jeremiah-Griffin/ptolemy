c4792bce662b23f9dbfb6286db39053f91622d3f
Jeremiah Griffin <jeremiahgriffin@protonmail.com>
2023-07-16 06:42:40 -0400

    added missing serde derives for clientt models

--- src/client.rs
+++ src/client.rs
@@ -1,7 +1,10 @@
+use serde::{Serialize, Deserialize};
+
 ///These types are used to represent mostly database models in a
 ///manner which doesn't leak any sensitive data or implementation details.
 /// They should be generated from the client side and sent to the server side exclusively.
 
+#[derive(Serialize, Deserialize)]
 pub struct ClientConsumerModel {
     pub address: String,
     pub email: String,
@@ -11,6 +14,7 @@ pub struct ClientConsumerModel {
     pub last_name: String,
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct ClientMerchantModel{
     pub address: String,
     pub email: String,
