95fe65cc8a563f71300e2530fea011a029f1a4c7
Jeremiah Griffin <jeremiahgriffin@protonmail.com>
2023-07-25 06:19:07 -0400

    added order request

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,4 @@
+use geoutils::Location;
 use serde::{Serialize, Deserialize};
 
 ///These types are used to represent mostly database models in a
@@ -5,11 +6,11 @@ use serde::{Serialize, Deserialize};
 /// They should be generated from the client side and sent to the server side exclusively.
 
 
-
-
-///This train current exists to mark which types User::new() should be generic over.
+///This trait current exists to mark which types User::new() should be generic over.
 pub trait ClientUserModel{}
 
+///represents all the data in a ConsumerModel (database model) that is safe
+///and useful for the client to consume.
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct ConsumerClientModel {
     pub address: String,
@@ -21,6 +22,9 @@ pub struct ConsumerClientModel {
 }
 impl ClientUserModel for ConsumerClientModel{}
 
+
+///represents all the data in a MerchantModel (database model) that is safe
+///and useful for the client to consume.
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct MerchantClientModel{
     pub address: String,
@@ -30,3 +34,10 @@ pub struct MerchantClientModel{
     pub name: String,
 }
 impl ClientUserModel for MerchantClientModel{}
+
+///Submitted to the server
+pub struct OrderRequest {
+    pub sixty_kg_bags_coffee: u32,
+    pub sixty_kg_bags_scraps: u32,
+    pub consumer_location: Location,
+}
